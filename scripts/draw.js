845a96fb1031fcecaa1a45e84c10659ecb1dd39e
Griffone <thegriffones@gmail.com>
2020-12-24 22:47:14 +0100

    Offset origin for interesting results

--- scripts/draw.js
+++ scripts/draw.js
@@ -2,8 +2,8 @@
 // Get global variables
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
-let LIMIT = 128;
-let SCALE = 1024;
+let LIMIT = 32;
+let SCALE = 1.2;
 
 // Setup listening functions
 // Invoke resizeCanvas() when the window is resized
@@ -18,14 +18,17 @@ function resizeCanvas() {
 	redraw();
 }
 
+// Add 2 complex numbers together
 function add(a, b) {
 	return [ a[0] + b[0], a[1] + b[1], ];
 }
 
+// Square (multiply by itself) a complex number
 function sqr(c) {
 	return [ c[0]*c[0] - c[1]*c[1], 2*c[0]*c[1], ];
 }
 
+// Get the square magnitude of a complex number
 function mag2(c) {
 	return c[0]*c[0] + c[1]*c[1];
 }
@@ -51,10 +54,11 @@ function redraw() {
 	let height = canvas.height;
 	let imageData = context.createImageData(width, height);
 	let pixels = imageData.data;
+	let xOffset = (height - width)*1.4;
 	for (var y = 0; y < height; y++) {
 		for (var x = 0; x < width; x++) {
 			let offset = (y * width + x) * 4;
-			let point = [ (x*2 - height) / height, (y*2 - height) / height, ];
+			let point = [ (x*2 - height + xOffset) / height * SCALE, (y*2 - height) / height * SCALE, ];
 			let pixel = mandelbrot(point, LIMIT);
             pixels[offset + 0] = pixel[0]; // red
 			pixels[offset + 1] = pixel[1]; // green
