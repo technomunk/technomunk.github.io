07d633ce128ec95a924c547be85ed9bb73c1f834
Griffone <thegriffones@gmail.com>
2021-09-12 16:03:05 +0200

    Commit missed files

---
+++ src/lib/util.ts
@@ -0,0 +1,15 @@
+interface Position {
+	x: number,
+	y: number,
+}
+
+/** Transform client-relative coordinates to dom-relative coordinates.
+ * @param client
+ * @param rect
+ * @returns relative position to the rect's origin in rect-space
+ */
+export function clientToRect(client: Position, rect: DOMRect): Position {
+	const x = (client.x - rect.x) / rect.width;
+	const y = (client.y - rect.y) / rect.height;
+	return { x, y };
+}
