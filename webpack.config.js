6f398179973be8bdf95e1e21bee7d164629a7733
Griffone <thegriffones@gmail.com>
2021-09-12 14:47:35 +0200

    Add game-of-life page

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 /* eslint-disable */
 
-path = require('path');
+const path = require('path');
 
 module.exports = {
 	entry: {
 		// cpu_main: ['./src/side_menu.ts', './src/cpu_mandelbrot/toggle_fullscreen.ts',],
 		// cpu_worker: './src/cpu_mandelbrot/draw_worker.ts',
 		comdyn: ['./src/comdyn.ts',],
-		gol: ['./src/gol.ts',],
+		gol: ['./src/gol_entry.ts',],
 	},
 	experiments: {
 		syncWebAssembly: true,
 		topLevelAwait: true,
 	},
-	// devtool: 'inline-source-map',
-	mode: 'production',
+	devtool: 'inline-source-map',
+	mode: 'development',
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'serve'),
+		},
+	},
 	module: {
 	  rules: [
 		{
@@ -29,6 +34,6 @@ module.exports = {
 	},
 	output: {
 		filename: '[name].js',
-		path: path.resolve(__dirname, 'docs'),
+		path: path.resolve(__dirname, 'serve'),
 	},
 };
