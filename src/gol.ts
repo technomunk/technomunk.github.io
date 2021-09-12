0ce2a5e3e1aba3735aa330c530ee324d312a7a63
Griffone <thegriffones@gmail.com>
2021-09-12 16:02:46 +0200

    Add basic interactivity

--- src/gol.ts
+++ src/gol.ts
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Universe } from "wasm-game-of-life/wasm_game_of_life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
+import { clientToRect } from "./lib/util";
 
 const canvas = document.getElementById("main-canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
 const CELL_SIZE = 16; // px
 
-const cellCntX = Math.floor(window.innerWidth / CELL_SIZE),
-cellCntY = Math.floor(window.innerHeight / CELL_SIZE);
+let cellCntX = Math.floor(window.innerWidth / CELL_SIZE);
+let cellCntY = Math.floor(window.innerHeight / CELL_SIZE);
 
 const COL_LIVE = '#000000';
 const COL_DEAD = '#FFFFFF';
@@ -17,21 +18,55 @@ const COL_GRID = '#CCCCCC';
 canvas.width = cellCntX * CELL_SIZE;
 canvas.height = cellCntY * CELL_SIZE;
 
-const universe = Universe.random(cellCntX, cellCntY);
+let universe = Universe.random(cellCntX, cellCntY);
+
+window.addEventListener('resize', () => {
+	cellCntX = Math.floor(window.innerWidth / CELL_SIZE);
+	cellCntY = Math.floor(window.innerHeight / CELL_SIZE);
+	canvas.width = cellCntX * CELL_SIZE;
+	canvas.height = cellCntY * CELL_SIZE;
+
+	universe.free();
+	universe = Universe.random(cellCntX, cellCntY);
+
+	requestAnimationFrame(() => {
+		drawGrid();
+		drawCells();
+	})
+})
+
+canvas.addEventListener('click', event => {
+	const canvasRect = canvas.getBoundingClientRect();
+	const { x, y } = clientToRect(event, canvasRect);
+
+	if (event.ctrlKey) {
+		universe.spawn_glider(Math.floor(x*cellCntX), Math.floor(y*cellCntY));
+	} else {
+		universe.toggle(Math.floor(x*cellCntX), Math.floor(y*cellCntY));
+	}
+	requestAnimationFrame(drawCells);
+});
+
+window.addEventListener('keypress', event => {
+	if (event.key == ' ') {
+		universe.tick();
+		requestAnimationFrame(drawCells);
+	}
+})
 
 function drawGrid() {
 	ctx.strokeStyle = COL_GRID;
 	
 	// Vertical lines.
 	for (let x = 0; x <= cellCntX; ++x) {
-		ctx.moveTo(x * (CELL_SIZE + 1) + 1, 0);
-		ctx.lineTo(x * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * cellCntY + 1);
+		ctx.moveTo(x * CELL_SIZE, 0);
+		ctx.lineTo(x * CELL_SIZE, CELL_SIZE * cellCntY);
 	}
 
 	// Horizontal lines.
 	for (let y = 0; y <= cellCntY; ++y) {
-		ctx.moveTo(0, y * (CELL_SIZE + 1) + 1);
-		ctx.lineTo((CELL_SIZE + 1) * cellCntX + 1, y * (CELL_SIZE + 1) + 1);
+		ctx.moveTo(0, y * CELL_SIZE);
+		ctx.lineTo(CELL_SIZE * cellCntX, y * CELL_SIZE);
 	}
 
 	ctx.stroke();
@@ -45,7 +80,6 @@ function cellAlive(cells: Uint8Array, x: number, y: number): boolean {
 }
 
 function drawCells() {
-	universe.tick();
 	const cells = new Uint8Array(memory.buffer, universe.cells_ptr(), universe.cells_size());
   
 	ctx.beginPath();
@@ -55,10 +89,10 @@ function drawCells() {
 			ctx.fillStyle = cellAlive(cells, x, y) ? COL_LIVE : COL_DEAD;
   
 			ctx.fillRect(
-				x * (CELL_SIZE + 1) + 1,
-				y * (CELL_SIZE + 1) + 1,
-				CELL_SIZE,
-				CELL_SIZE
+				x * CELL_SIZE,
+				y * CELL_SIZE,
+				CELL_SIZE - 1,
+				CELL_SIZE - 1
 			);
 		}
 	}
