32d51d9cbfaf5c981d5b695297fd88c5209ec5fe
Griffone <thegriffones@gmail.com>
2022-10-09 19:31:35 +0200

    Work on styling

--- src/script/lib/component/slidemenu.ts
+++ src/script/lib/component/slidemenu.ts
@@ -9,5 +9,26 @@ export default class SlideMenu extends HTMLElement {
 
         this.classList.add("slide-body")
         this.flap.classList.add("slide-flap")
+
+        this.positionFlap()
+        const repositionFlap = this.positionFlap.bind(this)
+        window.addEventListener("resize", repositionFlap)
+        this.addEventListener("resize", repositionFlap)
+    }
+
+    public get vertical(): boolean {
+        return window.innerHeight > window.innerWidth
+    }
+    
+
+    private positionFlap() {
+        if (window.innerWidth >= window.innerHeight) {
+            this.flap.style.bottom = ""
+            this.flap.style.right = `${this.clientWidth}px`
+        } else {
+            const rotationAdjustment = this.flap.clientHeight - 2*this.flap.clientWidth
+            this.flap.style.right = ""
+            this.flap.style.bottom = `${this.clientHeight - rotationAdjustment}px`
+        }
     }
 }
