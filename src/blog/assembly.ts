bb9504d3cc9d3558eb960489d5467411a9e2b30d
technomunk <thegriffones@gmail.com>
2023-09-09 00:57:09 +0200

    Clean up assembly ts

--- src/blog/assembly.ts
+++ src/blog/assembly.ts
@@ -1,20 +1,11 @@
-import hljs from 'highlight.js/lib/core'
-import x86asm from 'highlight.js/lib/languages/x86asm'
-
 import { InterpreterUI } from '../script/lib/interpreter/interpreter'
-import { CodeBlock, tokenize } from '../script/lib/interpreter/code'
+import { tokenize } from '../script/lib/interpreter/code'
 import './blog.css'
 
-// hljs.registerLanguage('x86asm', x86asm)
-// hljs.highlightAll()
-
-for (const interactive of document.querySelectorAll("code.interactive")) {
-    new InterpreterUI(interactive as HTMLElement)
-    console.log(interactive)
-}
-
-for (const nonInteractive of document.querySelectorAll("code:not(.interactive)")) {
-    if (nonInteractive.textContent) {
-        nonInteractive.innerHTML = tokenize(nonInteractive.textContent)
+for (const codeElement of document.querySelectorAll("code")) {
+    if (codeElement.classList.contains("interactive")) {
+        new InterpreterUI(codeElement)
+    } else if (codeElement.textContent) {
+        codeElement.innerHTML = tokenize(codeElement.textContent)
     }
 }
