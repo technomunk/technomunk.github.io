556827155a3ff85be847df74db123dc5551e8377
technomunk <thegriffones@gmail.com>
2023-09-23 19:51:40 +0200

    Write up compilers

--- src/plugins/remark/highlight.ts
+++ src/plugins/remark/highlight.ts
@@ -4,8 +4,13 @@ import { visit } from 'unist-util-visit'
 import { GRAMMARS } from '../../scripts/lib/code/grammar'
 import { markSemanticsWithSpans } from '../../scripts/lib/code/semanitcize'
 
-function highlightNode(node: Code | InlineCode, fallbackLang?: "bnfk" | "asm") {
-    node.value = `<code>${markSemanticsWithSpans(node.value, (node as any).lang || fallbackLang)}</code>`
+function highlightNode(node: Code | InlineCode, fallbackLang?: string) {
+    const lang = (node as any).lang || fallbackLang
+    if (GRAMMARS[lang] == undefined) {
+        console.warn(`Unknown language: "${lang}", skipping highlight`)
+        return
+    }
+    node.value = `<code>${markSemanticsWithSpans(node.value, lang)}</code>`
     if (node.type == "code") {
         node.value = `<pre>${node.value}</pre>`
     }
@@ -16,13 +21,9 @@ function highlightNode(node: Code | InlineCode, fallbackLang?: "bnfk" | "asm") {
 /** Custom code highlight plugin */
 export default function remarkHighlight(options = {}) {
     return (tree: Root, file: any) => {
-        const fallbackLang = file.data.astro.frontmatter.fallbackLang
-        const types = ["code"]
-        if (GRAMMARS[fallbackLang]) {
-            types.push("inlineCode")
-        } else {
-            console.warn("Unknown fallback language:", fallbackLang)
-        }
-        visit(tree, types, (node) => highlightNode(node as Code | InlineCode, fallbackLang))
+        visit(
+            tree,
+            ["code", "inlineCode"],
+            (node) => highlightNode(node as Code | InlineCode, file.data.astro.frontmatter.fallbackLang))
     }
 }
