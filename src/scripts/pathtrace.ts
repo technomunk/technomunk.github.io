4ae730045284b2617a6051ee3725cc8f0865f467
technomunk <thegriffones@gmail.com>
2024-11-14 13:40:04 +0100

    Make more beautiful

--- src/scripts/pathtrace.ts
+++ src/scripts/pathtrace.ts
@@ -13,22 +13,35 @@ function setup() {
     //     Math.ceil(window.innerHeight * ratio),
     // )
     const canvas = document.querySelector("canvas#main") as HTMLCanvasElement
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
 
     const renderer = new FragmentRenderer(canvas)
-    renderer.context.viewport(0, 0, canvas.width, canvas.height)
 
     renderer.setShader(PATHTRACE_SHADER)
     const setters = composeUniformSetters(renderer.context, renderer.program!)
 
-    let pos = new Vec3(2, .2, -.2);
+    function resize() {
+        const width = window.innerWidth * window.devicePixelRatio
+        const height = window.innerHeight * window.devicePixelRatio
+        canvas.style.width = `100%`
+        canvas.style.height = `100%`
+        canvas.width = width
+        canvas.height = height
+        renderer.context.viewport(0, 0, width, height)
+
+        setUniforms(setters, { uResolution: [width, height] })
+    }
+    resize()
+
+    let pos = new Vec3(2., .16, .2);
+    let fov = Math.PI / 5;
+    if (canvas.width < canvas.height) {
+        fov *= canvas.height / canvas.width;
+    }
 
     setUniforms(setters, {
-        uResolution: [canvas.width, canvas.height],
         uCamPos: pos,
         uView: Mat3.lookIn(Vec3.ZERO.sub(pos).normalize()),
-        uCamFov: Math.PI / 4,
+        uCamFov: fov,
         uSkybox: setupCubemap(renderer.context, { onComplete: () => renderer.draw() }),
     })
 
@@ -37,6 +50,7 @@ function setup() {
     gd.addDragObserver((d) => {
         pos = pos.rotate(Vec3.Y, d.dx / window.innerWidth * Math.PI)
         pos = pos.rotate(Vec3.cross(pos, Vec3.Y).normalize(), d.dy / window.innerHeight * Math.PI)
+        console.debug(pos)
         setUniforms(setters, {
             uCamPos: pos,
             uView: Mat3.lookIn(Vec3.ZERO.sub(pos).normalize()),
@@ -45,12 +59,7 @@ function setup() {
     })
 
     window.addEventListener("resize", () => {
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
-        renderer.context.viewport(0, 0, canvas.width, canvas.height)
-        setUniforms(setters, {
-            uResolution: [canvas.width, canvas.height],
-        })
+        resize()
         renderer.draw()
     })
     window.addEventListener("keypress", (e) => {
@@ -63,7 +72,7 @@ function setup() {
             return
         }
         pos.muli(1 + (e.deltaY / 1000))
-        setUniforms(setters, {uCamPos: pos})
+        setUniforms(setters, { uCamPos: pos })
         renderer.draw()
     })
 }
